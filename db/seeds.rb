5d511c8b0d414b61587c5c9a078299da51e772e7
Mike Yousef <msy@posteo.de>
2019-11-14 13:02:32 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning DB...'
+Ingredient.destroy_all
+
+puts 'Creating Ingredients'
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts 'Finished!'
