cb3b1a23ba477ef5b7abe1602a1c9e5ad97ca667
Mike Yousef <msy@posteo.de>
2019-11-14 16:55:00 +0100

    first half done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts 'Cleaning DB...'
-Ingredient.destroy_all
 
-puts 'Creating Ingredients'
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+puts 'Creating Cocktails'
+
+cocktail_attributes = [
+  { description: '1 part', ingredient_id: 108, cocktail_id: 1 },
+  { description: '2 part', ingredient_id: 107, cocktail_id: 2 },
+  { description: 'A handful', ingredient_id: 106, cocktail_id: 3 }
+]
+
+Dose.create!(cocktail_attributes)
 
 puts 'Finished!'
