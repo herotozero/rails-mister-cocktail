42cdbdb5c3744339a96ef3bfe074f54c7c996533
Mike Yousef <msy@posteo.de>
2019-11-15 12:40:59 +0100

    new version

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,14 +9,26 @@ class CocktailsController < ApplicationController
 
   def new
     @cocktail = Cocktail.new
+    # @ingredient = Ingredient.find(params[:id])
+    @ingredients = Ingredient.all
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
   end
 
   private
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
